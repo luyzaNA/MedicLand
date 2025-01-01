7880a6a7ea5432fae4e4ac9c9064ae2c4daca300
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-01-01 21:47:17 +0200

    Create user entity and configure auth process

---
+++ BE/src/Services/RegisterService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Specialization;
+use App\Entity\User;
+use App\Repository\SpecializationRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class RegisterService
+{
+    public function __construct(
+        private UserRepository $userRepository,
+        private SpecializationRepository $specializationRepository,
+        private  EntityManagerInterface $entityManager,
+        private  JWTTokenManagerInterface $jwtManager,
+        private  UserPasswordHasherInterface $passwordHasher,
+        private Security $security
+    ) {}
+
+    public function login(array $credentials): ?string
+    {
+        $email = $credentials['email'] ?? null;
+        $password = $credentials['password'] ?? null;
+
+        if (!$email || !$password) {
+            return null;
+        }
+
+        $user = $this->userRepository->findByEmail($email);
+
+
+        if (!$user || !$this->passwordHasher->isPasswordValid($user, $password)) {
+            return null;
+        }
+
+        return $this->jwtManager->create($user);
+    }
+
+    public function register(string $email, ?string $cnp = null, string $password, ?string $firstName = null, ?string $lastName = null, ?Specialization $specialization = null, string $role): array
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setCnp($cnp);
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setSpecialization($specialization);
+        $user->setRole($role);
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        $this->userRepository->save($user);
+
+        $token = $this->jwtManager->create($user);
+
+        return [
+            'status' => 'User registered successfully',
+            'token' => $token,
+        ];
+    }
+
+    public function getAuthUser(): ?User
+    {
+        $user = $this->security->getUser();
+
+        if ($user instanceof User) {
+            return $this->userRepository->findByEmail($user->getEmail());
+        }
+        return null;
+    }
+}
