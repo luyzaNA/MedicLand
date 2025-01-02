3d7b82d96be2c4a0d6fb18d868655cea8c7dadf5
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-01-02 21:11:25 +0200

    Add validation for consultation

--- BE/src/Controller/ConsultationController.php
+++ BE/src/Controller/ConsultationController.php
@@ -8,20 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
-
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 class ConsultationController extends AbstractController
 {
 
     public function __construct(private ConsultationService $consultationService, 
-                                private SerializerInterface $serializerInterface,
-                                private Security $security)
-    {
-    }
+                                private Security $security,
+                                private ValidatorInterface $validator
+    ){}
 
     #[Route('/api/consultation', name: 'add_consultationcon', methods: ['POST'])]
-
     public function addConsultation(Request $request): JsonResponse
     {
 
@@ -41,18 +39,45 @@ class ConsultationController extends AbstractController
 
         $data = json_decode( $request->getContent(), true);
 
+        $constraints = new Assert\Collection([
+            'patient' => [
+                new Assert\NotBlank()
+            ], 
+            'diseases' => [
+                new Assert\NotBlank()
+            ],
+            'medication' => [
+          new Assert\Optional(),
+                new Assert\Type('array'),
+                new Assert\All([
+                    new Assert\Type('string') 
+                ])
+            ],
+        ]);
+
+        $violations = $this->validator->validate($data, $constraints);
+
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                $errors[] = $violation->getPropertyPath() . ': ' . $violation->getMessage();
+            }
+            return new JsonResponse(['errors' => $errors], 400);
+        }
+
         if (!isset($data['patient'])) {
             return new JsonResponse( ['error' => 'Missing patient'], 400);
         }
 
         $patient = $data['patient'];
 
-        $date = new \DateTime($request->get('date'));
+        $date = new \DateTime(); 
 
-        $diseases = $request->get('diseases', []);
-
-        $medication = $request->get('medication', null);
-
+        // $medication = $data['medication'] ?? [];
+        
+        // $diseases = $request->get('diseases', []);
+        $diseases = $data['diseases'] ?? [];
+        $medication = $data['medication'] ?? [];
         try {
             $consultation = $this->consultationService->addConsultation(
                 $patient,
