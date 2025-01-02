d3af5b7b8e85562ffeba5ee7b69ac4038331601c
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-01-02 21:08:38 +0200

    Add validationn for user

--- BE/src/Controller/RegistrationController.php
+++ BE/src/Controller/RegistrationController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use App\Entity\User;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -21,16 +23,58 @@ class RegistrationController extends AbstractController
         private RegisterService $registerService,
         private SpecializationService $specializationService,
         private JWTTokenManagerInterface $jwtManager,
-        private Security $security       
+        private Security $security,
+        private ValidatorInterface $validator
+       
 ) {}
 
     #[Route('/api/register', name: 'api_register', methods: ['POST'])]
     public function register(Request $request): JsonResponse
     {
+        $user = $this->getUser();
+
+        if(!$user  instanceof User)     
+        {
+            return new JsonResponse (['error' => 'Missing user'], 400);
+        }          
+
+        $role = $user->getRoles();
+
+        if (!in_array('admin', $role)) {
+            return new JsonResponse(['error' => 'Unauthorized - Only admins can create account'], 403);
+        }
+
         $data = json_decode($request->getContent(), true);
+       
+        if (isset($data['role']) && $data['role'] === 'doctor') {
+            $constraints = new Assert\Collection([
+                'email' => [new Assert\NotBlank(), new Assert\Email()],
+                'password' => [new Assert\NotBlank(), new Assert\Length(['min' => 8])],
+                'role' => [new Assert\NotBlank()],
+                'cnp' => [new Assert\NotBlank(), new Assert\Length(['min' => 13, 'max' => 13]), new Assert\Regex('/^\d{13}$/')],
+                'firstName' => [new Assert\NotBlank()],
+                'lastName' => [new Assert\NotBlank()],
+                'specialization' => [new Assert\NotBlank()]
+            ]);
+        }else if(isset($data['role']) && $data['role'] === 'admin') {
+            $constraints = new Assert\Collection([
+                'email' => [new Assert\NotBlank(), new Assert\Email()],
+                'password' => [new Assert\NotBlank(), new Assert\Length(['min' => 8])],
+                'role' => [new Assert\NotBlank()]
+            ]);
+        }
+
+        $violations = $this->validator->validate($data, $constraints);
+
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                $errors[] = $violation->getPropertyPath() . ': ' . $violation->getMessage();
+            }
+            return new JsonResponse(['errors' => $errors], 400);
+        }
 
         try {
-
             $specialization = null;
             if (isset($data['specialization'])) {
                 $specialization = $this->specializationService->getSpecialization($data['specialization']);
@@ -38,6 +82,7 @@ class RegistrationController extends AbstractController
                     throw new \Exception('Specialization not found.');
                 }
             }
+
             $response = $this->registerService->register(
                 $data['email'],
                 $data['cnp'] ?? null,
@@ -48,7 +93,6 @@ class RegistrationController extends AbstractController
                 $data['role']
             );
 
-
             return new JsonResponse($response, 201);
         } catch (\Exception $e) {
             return new JsonResponse(['error' => $e->getMessage()], 400);
@@ -58,26 +102,37 @@ class RegistrationController extends AbstractController
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
     public function login(Request $request): JsonResponse
     {
-
         $data = json_decode($request->getContent(), true);
+
+        $constraints = new Assert\Collection([
+            'email' => [new Assert\NotBlank(), new Assert\Email()],
+            'password' => [new Assert\NotBlank()],
+        ]);
+    
+        $violations = $this->validator->validate($data, $constraints);
+    
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                $errors[] = $violation->getPropertyPath() . ': ' . $violation->getMessage();
+            }
+            return new JsonResponse(['errors' => $errors], 400);
+        }
 
         $token = $this->registerService->login([
             'email' => $data['email'],
             'password' => $data['password']
         ]);
 
-
         if (!$token) {
             return new JsonResponse(['error' => 'Invalid credentials'], 401);
         }
-
         return new JsonResponse(['token' => $token], 200);
     }
 
     #[Route('/api/auth/user', name: 'api_get_auth_user', methods: ['GET'])]
     public function getAuthUserDetails(): JsonResponse
     {
-
         $user = $this->getUser();        
         if (!$user instanceof User) {
             return new JsonResponse(['error' => 'Unauthorized - No authenticated user'], 401);
@@ -89,7 +144,6 @@ class RegistrationController extends AbstractController
             'roles' => $user->getRoles(),
             'firstName' => $user->getFirstName(),
             'lastName' => $user->getLastName(),
-
             'specialization' => $user->getSpecialization()->getName(),  
         ]);
     }
